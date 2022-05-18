5ae2d3083889aee7357ee69c68cb6fcbd9887992
Vijayalaxmi Kaleguddi <105513346+Vijayalaxmi1998@users.noreply.github.com>
2022-05-18 23:55:09 +0530

    Add files via upload

---
+++ vijayalaxmi.py
@@ -0,0 +1,17 @@
+#using print()
+print("hello python")
+print(123)
+x=2
+print(x)
+
+#using type
+a=3
+print(type(a))
+b="python"
+print(type(b))
+c=5.6
+print(type(c))
+
+#using input()
+a=float(input("Enter the value of A :"))
+print("A :",a)
